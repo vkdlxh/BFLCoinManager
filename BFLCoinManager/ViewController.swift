06f9d855e7247bd584920eb2d351d4c025d0e782
jaeeun <leejaeeun@archive-asia.co.jp>
2018-01-30 21:58:36 +0900

    first commit

---
+++ BFLCoinManager/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BFLCoinManager
+//
+//  Created by jaeeun on 2018/01/30.
+//  Copyright © 2018年 leejaeeun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
